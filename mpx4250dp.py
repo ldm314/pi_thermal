8f7164d2b8c3b36630f86b2ca035d50a7bd42247
Brian Moore <brian@briansblog.net>
2016-04-11 08:40:26 +0000

    add dp sensor, tweak for my sensor/alt

---
+++ mpx4250dp.py
@@ -0,0 +1,22 @@
+import adc0831
+
+class MPX4250DP (object):
+  def __init__(self):
+    self.sensor = adc0831.ADC0831()
+
+  def getKpa(self):
+    val = self.sensor.getVolts()
+    offset = 0.192
+    print "val - offset: {}".format(val - offset)
+    return ((val - offset))/0.00369
+
+if __name__ == "__main__":
+  import time
+  import os
+
+  s = MPX4250DP()
+
+  while True:
+    print "kPa: {}".format(s.getKpa())
+    time.sleep(1)
+
